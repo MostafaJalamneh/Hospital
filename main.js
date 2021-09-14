b8fb8eaf9db932b934876be76352c70ec952a2c2
Your Name <mostafa.jalamneh17@gmail.com>
2021-09-14 16:07:28 +0300

    Finish version one

---
+++ main.js
@@ -0,0 +1,44 @@
+const booking = () => {
+    var modal = document.getElementById("myModal");
+    var book = document.getElementById("booking");
+    var span = document.getElementsByClassName("close")[0];
+
+    book.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 5000);
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
